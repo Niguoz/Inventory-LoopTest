f8ab2d9ade9fc3b967155a53614bb635f06e11c7
Nicola Pellegrini <90765299+Niguoz@users.noreply.github.com>
2022-10-24 11:49:57 +0200

    Initial commit without content

---
+++ Source/SPProject/SPProject.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SPProject : ModuleRules
+{
+	public SPProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
---
+++ Source/SPProject/SPProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SPProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASPProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASPProjectGameMode();
+};
+
+
+
